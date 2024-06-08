20adb4ce11603b0e648d6d439532b606a5bdacf9
Amanda Munhoz Crozariol <158174011+MandyCro@users.noreply.github.com>
2024-06-08 13:49:46 +0000

    teste

---
+++ soma.py
@@ -0,0 +1,7 @@
+def somar_numeros(numero1, numero2):
+    soma = numero1 + numero2
+    return soma
+
+# Exemplo de uso da função
+resultado = somar_numeros(5, 3)
+print("O resultado da soma é:", resultado)
