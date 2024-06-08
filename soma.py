7368fe1e2b0165f402a5fe729adaa7cbe57be344
Amanda Munhoz Crozariol <158174011+MandyCro@users.noreply.github.com>
2024-06-08 14:08:32 +0000

    segundo commit

--- soma.py
+++ soma.py
@@ -5,3 +5,27 @@ def somar_numeros(numero1, numero2):
 # Exemplo de uso da função
 resultado = somar_numeros(5, 3)
 print("O resultado da soma é:", resultado)
+
+import unittest
+
+def somar_numeros(numero1, numero2):
+    soma = numero1 + numero2
+    return soma
+
+class TestSomarNumeros(unittest.TestCase):
+    def test_somar_numeros(self):
+        # Teste com números positivos
+        self.assertEqual(somar_numeros(5, 3), 8)
+        
+        # Teste com números negativos
+        self.assertEqual(somar_numeros(-5, -3), -8)
+        
+        # Teste com um número positivo e outro negativo
+        self.assertEqual(somar_numeros(5, -3), 2)
+        
+        # Teste com zero
+        self.assertEqual(somar_numeros(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
+
